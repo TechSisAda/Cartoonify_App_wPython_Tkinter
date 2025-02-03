54d3d7bb50b388b5e36ae7dfe71397b245415233
Adaeze Nnonyelu <nnonyeluadaeze@gmail.com>
2025-02-03 14:00:46 +0100

    added new files

---
+++ README2.md
@@ -0,0 +1,55 @@
+                        INDEX 4
+First you import the necesary libraries:
+    cv2 is OpenCV library for handling image processing tasks. It helps with reading the images, processing them and performing operations like filtering or edge detetction.
+    Image is a part of the PIL (PILLOW) library used for handling  image files and saving them in different formats.
+    numpy is a library essntial for handling arrays, which is how images are stored (as matrices of pixels).
+
+SECOND: Define the Cartoonify function:
+    the function CARTOONIFY takes two arg: IMAGE_PATH to show the location of the image and OUTPUT_PATH to show where to save the cartooned image.
+
+THIRD Load, Read and check the image
+    cv2.imread is for reading the image from the given path. Here the image is converted into a format that OpenCV can process.
+    checks if the image is loaded correctly or if the path is incorrect or corrupt.
+    it then returns an error message if there's any, and leaves the function.
+
+FOURTH Resize Image:
+    Resize the image in order to speed up the subsequent processing
+
+FIFTH Apply Bilateral Filter for Smoothing Images and Reducing Noise still keeping the Edges
+    The FOR loop applys bilateral filter 3 times to the image.
+    The underscore is a place-holder (i dont care about the loop index, just that it runs 3 times)
+    This bilateral filter is a type of filter that reduces the noise and keeps edges sharp, this is cruical for the cartoon effect.
+    D, SIGMA COLOR, and SIGMA SPACE are parameters of the filters operation.
+    d=9:Diameter of each pixel neighborhood.
+    SIGMA COLOR decides which colors to merge and which to keep separate. the smaller it is, the more edges preserved.
+    SigmaColor=20: How much to smooth the colors.
+    SigmaSpace=20: How much to smooth the spatial details.
+
+
+SIXTH Convert to Grayscale and Apply Medium Blur:
+    cvtColor converts the color image to grayscale for creating the cartoon-like edges (edge detection).
+    medianBlur reduces noise on the grayscale in order to improve the quality of edge detection. 5 show the size of the filter applied to the pixels.
+
+SEVENTH Adaptive Thresholding for Edge Detection:
+    It creates a mask of the edges in the image, just ike drawing the outlines of the image.
+    255: maximum intensity for the edges(white)
+    Adaptive_thresh_mean_c: this method looks at the small regions of the image to determine if theyshould be white(edge) or black(not an edge).
+    thresh_binary: it applies a binary threshold(black or white)
+    blocksize=9: size of the area used to calculate the threshold.
+    c=4: a constant subtracted from the mean to fine-tune edge detection.
+
+EIGHTH Reapply Bilateral Filter to Smooth Color Image
+    reapplying the bilateral filter to the original image to further enhance the smoothness for the final cartoon effect. The effects of smoothing of this one are stronger than the first one.
+
+NINTH Combine Edges and Color Image
+    It combines the edges(black and white lines) with the color image, like coloring inside the cartoon outlines.
+    mask=edges: ensures that the edges are apllied to the color image, therefore creating the cartoon effect. 
+
+TENTH Convert back to RGB and Save the Image
+    OpenCV uses BGR(Blue-Green-Red) format for colors but other image libraries use RGB(Red-Green-Blue).
+    cvtcolor converts the image from BGR, which is the default color format for OpenCV, to RGB.
+    Image.fromarray converts the array representation of the image into a PIL Image Object(which PIL understands).
+    Then save the image to a specific path(output_path) in a suitable format(JPEG in this case).
+
+LASTLY Call the cartoonify function with the specified paths to the input and output files. Telling it to process the image found at the image_path and save the cartoon version to the output_path.
+    
--- tkinterCartoon.py
+++ tkinterCartoon.py
@@ -2,8 +2,7 @@ import tkinter as tk
 from tkinter import filedialog, messagebox
 import cv2
 from PIL import Image, ImageTk
-import numpy as np 
-import os
+
 
 #the Class handling loading of the images.
 class ImageLoader:
@@ -13,8 +12,9 @@ class ImageLoader:
     def load_image(self):
         #set the initial directory to the folder you want to open
         initial_folder = r"C:\Users\ADA NNONYELU\Desktop\dummy_folder"
-        # opens file dialog in order to choose an image starting with the specified folder
-        self.file_path = filedialog.askopenfilename(initialdir=initial_folder, filetypes=[("Image files", "*.jpeg;*.jpg;*.png")]) #to limit the files for this application to just image files(jpeg &png)
+        
+        # opens file dialog in order to choose an image starting with the specified folder(dummy folder)
+        self.file_path = filedialog.askopenfilename(initialdir=initial_folder) #filetypes=[("Image files", "*.jpeg;*.jpg;*.png")]) #to limit the files for this application to just image files(jpeg &png)
         
         if self.file_path:
             if not (self.file_path.endswith('.jpeg') or self.file_path.endswith('.jpg') or self.file_path.endswith('.png')):
@@ -30,18 +30,20 @@ class Cartoonifier: #class to process the image selected and give it a cartoon-l
     
     def cartoonify_image(self):
         img =cv2.imread(self.image_path) #to reads the image form the file path
-        img = cv2.resize(img, (850,850)) # to resize the image to 650:600
+        img = cv2.resize(img, (850,850)) # to resize the image to 850:850
 
-        for _ in range(4):
-            img = cv2.bilateralFilter(img, d=9, sigmaColor=15, sigmaSpace=2)
+        for _ in range(8): 
+            img = cv2.bilateralFilter(img, d=9, sigmaColor=15, sigmaSpace=12) #applies a bilateral filter 4 times to smooth the images while still preserving the edges.
+                    #d = diameter of each pixel; sC = the smaller the num, the more edges preserved; sS = smooths spatial details
 
-        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) # converting to gray scale
-        gray = cv2.medianBlur(gray, 5) #reduces noise of the gray scale
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) # converting to gray scale to create edges
+        gray = cv2.medianBlur(gray, 5) #reduces noise of the gray scale, improving the quality of the edge detection, 5 is the size of the filter.
 
         # edge detection: what this does is to create black and white lines(outlines) using adaptive threshold. the cartoon is therefore visible
-        edge = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, blockSize=5, C=2)
+        edge = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, blockSize=5, C=2) #bS must be an odd num; c = finetunes the edges
 
-        cartoon =cv2.bitwise_and(img, img, mask=edge)# this combines the edge and original image together. keeping the color in non-edge areas and making the edges darker.
+        cartoon =cv2.bitwise_and(img, img, mask=edge)# this combines the edge and original image together. keeping the color in non-edge areas and making the edges darker;
+                                                     #mask=edges ensures the edges are applied to the color image.
 
         cartoon = cv2.cvtColor(cartoon, cv2.COLOR_BGR2RGB) #OpenCV works with BGR but PIL accepts RGB, so the image is converted.
         return Image.fromarray(cartoon) #the processed image is returned as a PIL image object
@@ -59,59 +61,58 @@ class FileSaver:
 
 class CartoonifyApp:
     def __init__(self, root):
-        self.root = root 
+        self.root = root #initializes the main window; root
         self.root.title("Cartoonify Your Pictures")
-        self.root.geometry("800x700+200+200")
-        self.image_label = None
+        self.root.geometry("800x700")
+        self.image_label = None #initialized as None to hold the image in the GUI later.
 
     #Buttons:
         self.load_button = tk.Button(root, text="Load Image", command=self.load_image)
-        self.load_button.pack(pady=10)
+        self.load_button.pack(pady=15) #creates a *load_image* button that calls the load_image method when clicked
 
         self.cartoonify_button = tk.Button(root, text="Cartoonify Image", command=self.cartoonify_image,state=tk.DISABLED)
-        self.cartoonify_button.pack(pady=10)
+        self.cartoonify_button.pack(pady=15)#creates a cartoonify image which is disabled in the beginning until an image is loaded, applies when clicked.
 
         self.save_button = tk.Button(root, text="Save Image", command=self.save_image, state=tk.DISABLED)
-        self.save_button.pack(pady=10)
+        self.save_button.pack(pady=15)#creates a save button, disabled initialy until a cartooned image is generated
 
         self.loaded_image = None
      
     def load_image(self):
-        loader = ImageLoader()
+        loader = ImageLoader() #uses the ImageLoader class to load the image
         file_path = loader.load_image()
 
         if file_path:
-            self.loaded_image = Image.open(file_path)
-            self.display_image(self.loaded_image)
-            self.cartoonify_button.config(state=tk.NORMAL)
+            self.loaded_image = Image.open(file_path) #if successful,
+            self.display_image(self.loaded_image)#it displays the image 
+            self.cartoonify_button.config(state=tk.NORMAL)#enabling the caroonify image button
 
 
     def cartoonify_image(self):
         if self.loaded_image:
-            cartoonifier =Cartoonifier(self.loaded_image.filename)
-            self.cartoon_image = cartoonifier.cartoonify_image()
-            self.display_image(self.cartoon_image)
-            self.save_button.config(state=tk.NORMAL)
+            cartoonifier =Cartoonifier(self.loaded_image.filename) #usesthe cartoonifier class
+            self.cartoon_image = cartoonifier.cartoonify_image() #to apply the cartoon effect
+            self.display_image(self.cartoon_image)# displays it 
+            self.save_button.config(state=tk.NORMAL) # and then the save image button is enabled.
 
     def save_image(self):
         if self.cartoon_image:
-            saver = FileSaver()
-            saver.save_image(self.cartoon_image)
+            saver = FileSaver()# uses the File Saver class 
+            saver.save_image(self.cartoon_image) #to save the cartoonified image
+    
+    def display_image(self,img): #Display Image method
+        img.thumbnail((500, 500)) # Resizes the iamge to fit the display area.
+        img_tk = ImageTk.PhotoImage(img) #Converting it to a format suitable to display on Tkinter.
 
-    #Display Image
-    def display_image(self,img):
-        img.thumbnail((300, 300)) # RESIZE FOR DISPLAY
-        img_tk = ImageTk.PhotoImage(img)
-
-        if self.image_label is None:
+        if self.image_label is None: #if no image is loaded, a label is created to display the image, if an image is already shown, it updates the label
             self.image_label= tk.Label(self.root, image=img_tk)
-            self.image_label.image = img_tk
+            self.image_label.image = img_tk #assigns img_tk to the label
             self.image_label.pack(pady=10)
         else:
             self.image_label.config(image=img_tk)
             self.image_label.image = img_tk
 
-if __name__ == "__main__":
-            root = tk.Tk() #Create the main window
-            app = CartoonifyApp(root) #Create an instance of
-            root.mainloop()
+#if __name__ == "__main__": #it checks if this script is run directly or if i imported it from another module
+root = tk.Tk() #Create the main window
+app = CartoonifyApp(root) #Create an instance of the CartoonifyApp class managing everything related to the app's behaviour inside tkinter window
+root.mainloop() #Starts the program and would close only after the window is closed
